19d79becd0766310f0cba82985c05d925a9ea7c4
strawpple <akrequillo@gmail.com>
2025-07-24 10:28:52 +0800

    updated

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PortfolioIntro from './ProfileCard';
+
+function App() {
+  return (
+    <div>
+      <PortfolioIntro />
+    </div>
+  );
+}
+
+export default App;
